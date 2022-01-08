60f086ebc1646ebeb0dbf309511e7206b7cbe5e0
Mary Gibbs <mgibbs1259@gmail.com>
2022-01-08 11:38:08 -0500

    old version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import Tour from "./components/pages/Tour";
+import Merch from "./components/pages/Merch";
+import SignUp from "./components/pages/SignUp";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/tour" element={<Tour />} />
+					<Route path="/merch" element={<Merch />} />
+					<Route path="/sign-up" element={<SignUp />} />
+				</Routes>
+			</Router>
+		</>
+	);
 }
 
 export default App;
