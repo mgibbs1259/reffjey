f7a754e744c961344d29c344f16ebbe2cde7bd64
Mary Gibbs <mgibbs1259@gmail.com>
2022-01-14 18:10:16 -0500

    animation

--- src/components/MainSecondary.js
+++ src/components/MainSecondary.js
@@ -2,6 +2,8 @@ import React from "react";
 import { motion } from "framer-motion";
 import "../App.css";
 import "./Main.css";
+import CardImageItem from "./CardImageItem";
+import "./Cards.css";
 
 function AnimateTrackTitle({ delay, value }) {
 	return (
@@ -35,22 +37,54 @@ export default function MainSecondary() {
 			>
 				<h1>the beautiful sound of anything</h1>
 			</motion.div>
-			<AnimateTrackTitle
-				delay={2}
-				value={"can't let go anymore"}
-			></AnimateTrackTitle>
-			<AnimateTrackTitle
-				delay={3}
-				value={"floating to the surface"}
-			></AnimateTrackTitle>
-			<AnimateTrackTitle
-				delay={4}
-				value={"everything in light"}
-			></AnimateTrackTitle>
-			<AnimateTrackTitle
-				delay={5}
-				value={"the beautiful sound of anything"}
-			></AnimateTrackTitle>
+			<div className="cards__container">
+				<div className="cards__wrapper">
+					<ul className="cards__items">
+						<motion.div
+							initial="hidden"
+							whileInView="visible"
+							viewport={{ once: true }}
+							transition={{ duration: 4 }}
+							variants={{
+								visible: { opacity: 1 },
+								hidden: { opacity: 0 },
+							}}
+						>
+							<CardImageItem
+								src="/images/the-beautiful-sound-of-anything-album-cover.jpg"
+								href="https://open.spotify.com/album/5LiS0sYeOXXOZAbYj47WwJ?si=ZowSx70SQsS603imJl_G0Q"
+							/>
+						</motion.div>
+
+						<li className="cards__item">
+							<div className="cards__item__box">
+								<AnimateTrackTitle
+									delay={2}
+									value={"can't let go anymore"}
+									className=".cards__item__info"
+								></AnimateTrackTitle>
+								<AnimateTrackTitle
+									delay={3}
+									value={"floating to the surface"}
+									className=".cards__item__info"
+								></AnimateTrackTitle>
+								<AnimateTrackTitle
+									delay={4}
+									value={"everything in light"}
+									className=".cards__item__info"
+								></AnimateTrackTitle>
+								<AnimateTrackTitle
+									delay={5}
+									value={
+										"the beautiful sound of anything"
+									}
+									className=".cards__item__info"
+								></AnimateTrackTitle>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 }
