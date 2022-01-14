02a472e3d067220e3724a4acb3ca7555e66029c4
Mary Gibbs <mgibbs1259@gmail.com>
2022-01-14 18:30:11 -0500

    animation

--- src/components/MainSecondary.js
+++ src/components/MainSecondary.js
@@ -37,52 +37,55 @@ export default function MainSecondary() {
 			>
 				<h1>the beautiful sound of anything</h1>
 			</motion.div>
-			<div className="cards__container">
-				<div className="cards__wrapper">
-					<ul className="cards__items">
-						<motion.div
-							initial="hidden"
-							whileInView="visible"
-							viewport={{ once: true }}
-							transition={{ duration: 4 }}
-							variants={{
-								visible: { opacity: 1 },
-								hidden: { opacity: 0 },
-							}}
-						>
-							<CardImageItem
-								src="/images/the-beautiful-sound-of-anything-album-cover.jpg"
-								href="https://open.spotify.com/album/5LiS0sYeOXXOZAbYj47WwJ?si=ZowSx70SQsS603imJl_G0Q"
-							/>
-						</motion.div>
-
-						<li className="cards__item">
-							<div className="cards__item__box">
-								<AnimateTrackTitle
-									delay={2}
-									value={"can't let go anymore"}
-									className=".cards__item__info"
-								></AnimateTrackTitle>
-								<AnimateTrackTitle
-									delay={3}
-									value={"floating to the surface"}
-									className=".cards__item__info"
-								></AnimateTrackTitle>
-								<AnimateTrackTitle
-									delay={4}
-									value={"everything in light"}
-									className=".cards__item__info"
-								></AnimateTrackTitle>
-								<AnimateTrackTitle
-									delay={5}
-									value={
-										"the beautiful sound of anything"
-									}
-									className=".cards__item__info"
-								></AnimateTrackTitle>
-							</div>
-						</li>
-					</ul>
+			<div className="cards">
+				<div className="cards__container">
+					<div className="cards__wrapper">
+						<ul className="cards__items">
+							<motion.div
+								initial="hidden"
+								whileInView="visible"
+								viewport={{ once: true }}
+								transition={{ duration: 4 }}
+								variants={{
+									visible: { opacity: 1 },
+									hidden: { opacity: 0 },
+								}}
+							>
+								<CardImageItem
+									src="/images/the-beautiful-sound-of-anything-album-cover.jpg"
+									href="https://open.spotify.com/album/5LiS0sYeOXXOZAbYj47WwJ?si=ZowSx70SQsS603imJl_G0Q"
+								/>
+							</motion.div>
+							<li className="cards__item">
+								<div className="cards__item__box">
+									<AnimateTrackTitle
+										delay={2}
+										value={"can't let go anymore"}
+										className=".cards__item__info"
+									></AnimateTrackTitle>
+									<AnimateTrackTitle
+										delay={3}
+										value={
+											"floating to the surface"
+										}
+										className=".cards__item__info"
+									></AnimateTrackTitle>
+									<AnimateTrackTitle
+										delay={4}
+										value={"everything in light"}
+										className=".cards__item__info"
+									></AnimateTrackTitle>
+									<AnimateTrackTitle
+										delay={5}
+										value={
+											"the beautiful sound of anything"
+										}
+										className=".cards__item__info"
+									></AnimateTrackTitle>
+								</div>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</div>
