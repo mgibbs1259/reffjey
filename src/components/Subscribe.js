895c3b54782803358b66914f8ce2232ba6874485
Mary Gibbs <mgibbs1259@gmail.com>
2022-01-28 21:21:04 -0500

    changes

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -2,15 +2,22 @@ import React from "react";
 import "./Button.css";
 import "./Subscribe.css";
 
-export default function Subscribe(props) {
+export default function Subscribe() {
 	return (
 		<>
 			<div className="subscribe-container">
 				<form
+					name="contact"
 					method="POST"
 					data-netlify="true"
 					netlify-honeypot="bot-field"
 				>
+					<p class="hidden">
+						<label>
+							Don’t fill this out if you’re human:{" "}
+							<input name="bot-field" />
+						</label>
+					</p>
 					<input
 						className="subscribe-input"
 						placeholder="email address"
